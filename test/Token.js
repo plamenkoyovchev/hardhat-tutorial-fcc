06279fcd1281f619c6bbee07f01a15269c7d815f
plamenkoyovchev <plamenkooo@gmail.com>
2022-11-26 20:49:36 +0200

    Add more tests.

--- test/Token.js
+++ test/Token.js
@@ -51,4 +51,39 @@ describe("Token contract", function () {
 			hardhatToken.connect(addr1).transfer(addr2.address, 50)
 		).to.changeTokenBalances(hardhatToken, [addr1, addr2], [-50, 50]);
 	});
+
+	it("should emit Transfer events", async function () {
+		const { hardhatToken, owner, addr1, addr2 } = await loadFixture(
+			deployTokenFixture
+		);
+
+		// Transfer 50 tokens from owner to addr1
+		await expect(hardhatToken.transfer(addr1.address, 50))
+			.to.emit(hardhatToken, "Transfer")
+			.withArgs(owner.address, addr1.address, 50);
+
+		// Transfer 50 tokens from addr1 to addr2
+		// We use .connect(signer) to send a transaction from another account
+		await expect(hardhatToken.connect(addr1).transfer(addr2.address, 50))
+			.to.emit(hardhatToken, "Transfer")
+			.withArgs(addr1.address, addr2.address, 50);
+	});
+
+	it("Should fail if sender doesn't have enough tokens", async function () {
+		const { hardhatToken, owner, addr1 } = await loadFixture(
+			deployTokenFixture
+		);
+		const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
+
+		// Try to send 1 token from addr1 (0 tokens) to owner.
+		// `require` will evaluate false and revert the transaction.
+		await expect(
+			hardhatToken.connect(addr1).transfer(owner.address, 1)
+		).to.be.revertedWithCustomError(hardhatToken, "NotEnoughTokens");
+
+		// Owner balance shouldn't have changed.
+		expect(await hardhatToken.balanceOf(owner.address)).to.equal(
+			initialOwnerBalance
+		);
+	});
 });
